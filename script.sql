91bdc7e037f1e0828719a10e8ec47c7d87bd58b4
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-11-17 13:17:27 -0300

    aa
    aa

---
+++ script.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `contato` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`nome` varchar(50) NOT NULL,
+`telefone` varchar(50) NULL,
+`email` varchar(50) NULL,
+`endereco` TEXT NULL,
+PRIMARY KEY (`id`)
+);
