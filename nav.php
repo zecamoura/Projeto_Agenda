493478edd8c32ec7c7d9c45247d166281b9ca103
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-11-17 23:53:52 -0300

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,13 +1,11 @@
 <nav class="navbar navbar-nav">
     <div class="container">
         <ul class="nav navbar-nav">
-            <li><a href="javascript:void(0);" id="listar_contato"><i 
-            class="glyphicon glyphicon-th-list"></i>Listar</a></li>
-            <li><a href="javascript:void(0);" id="novo_contato"><i 
-            class="glyphicon glyphicon-plus"></i>Novo</a></li>
+            <li><a href="javascript:void(0);" id="listar_contato"><i class="glyphicon glyphicon-th-list"></i>Listar</a></li>
+            <li><a href="javascript:void(0);" id="novo_contato"><i class="glyphicon glyphicon-plus"></i>Novo</a></li>
         </ul>
     </div>
 </nav>
 <div id="carregando" style="display: none; text-align: center;">
-<img src="./resources/common/images/carregando.gif" />
+    <img src="./resources/lib/bootstrap/images/carregando.gif" />
 </div>
