5be2373c48d7c8344f4900c926ee19030375ac07
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-11-17 23:15:49 -0300

    Update cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content = "IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crud Contato - PHP Profissional</title>
+    <title>CRUD Contato - PHP Profissional</title>
     <link href="../resources/lib/bootstrap/css/bootstrap.css"
     rel = "stylesheet">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="jumbotron"> 
+            <h1>CRUD - Contato - PHP Professional</h1>
+    </div>
 </body>
 </html>
