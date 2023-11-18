22faa3013b1c96993c3ed5f7bf83df479b6b93ab
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-11-18 00:15:06 -0300

    Update conteudo.php

--- conteudo.php
+++ conteudo.php
@@ -1,1 +1,21 @@
 <div id="conteudo"></div>
+<div id="excluir_contato_modal" class="modal fade" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                    <span aria-hidden="true">&times;</span>
+                </button> 
+                <h4 class="modal-title">Excluir Contato</h4> 
+            </div>
+            <div class="modal-body">
+                <p>Confirma a exclus&atilde;o do contato?</p>
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" id="contato_id" value="" />
+                <button type="button" id="excluir" class="btn btn-primary">Sim</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal"> N&atilde;o</button>
+            </div>
+        </div>
+    </div>
+</div>
