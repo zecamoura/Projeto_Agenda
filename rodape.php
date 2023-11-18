dda42b2c4d596045d983bc358c6fa78d75ddf417
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-11-18 00:23:32 -0300

    a
    a

--- rodape.php
+++ rodape.php
@@ -1,2 +1,4 @@
 <script type="text/javascript"
-src="./resources/lib/jQuery/js/jq"
+src="./resources/lib/jQuery/js/jquery-3.1.1.js"></script>
+<script type="text/javascript"
+src="./resources/lib/bootstrap/js"
