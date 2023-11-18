e88dc8a8dbf42c718843fdfc40e2bcb4ac221cbf
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-11-17 23:42:02 -0300

    a
    a

---
+++ nav.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-nav">
+    <div class="container">
+        <ul class="nav navbar-nav">
+            <li><a href="javascript:void(0);" id="listar_contato"><i 
+            class="glyphicon glyphicon-th-list"></i>Listar</a></li>
+            <li><a href="javascript:void(0);" id="novo_contato"><i 
+            class="glyphicon glyphicon-plus"></i>Novo</a></li>
+        </ul>
+    </div>
+</nav>
+<div id="carregando" style="display: none; text-align: center;">
+<img src="./resources/common/images/carreganfo.gif" />
+</div>
